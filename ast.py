103d6e76af369266e13c7c8e0046adfb77e147a9
Nic Bricknell <nic@bricknell.io>
2018-03-17 13:51:06 +0000

    Fix AST object names

--- ast.py
+++ ast.py
@@ -2,49 +2,49 @@ class Ast:
     pass
 
 
-class AstIdent(Ast):
+class Ident(Ast):
     def __init__(self, ident):
         self.ident = ident
 
 
-class AstSum(Ast):
+class Sum(Ast):
     def __init__(self, left, right):
         self.left = left
         self.right = right
 
 
-class AstFnDef(Ast):
+class FnDef(Ast):
     def __init__(self, name, arguments, body_statements):
         self.name = name
         self.arguments = arguments
         self.body_statements = body_statements
 
 
-class AstReturn(Ast):
+class Return(Ast):
     def __init__(self, expr):
         self.expr = expr
 
 
-class AstFnCall(Ast):
+class FnCall(Ast):
     def __init__(self, name, arguments):
         self.name = name
         self.arguments = arguments
 
 
-class AstLet(Ast):
+class Let(Ast):
     def __init__(self, name, value):
         self.name = name
         self.value = value
 
 
-class AstIf(Ast):
+class If(Ast):
     def __init__(self, condition, if_body_statements, else_body_statements):
         self.condition = condition
         self.if_body_statements = if_body_statements
         self.else_body_statements = else_body_statements
 
 
-class AstWhile(Ast):
+class While(Ast):
     def __init__(self, condition, statements):
         self.condition = condition
         self.statements = statements
